a79e56f055b99adbc956b729b801699f4e791e04
Вероника Арцимовская <nika301030@gmail.com>
2023-02-12 14:07:22 +0300

    404 страница и рефакторинг

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,13 @@ import {createBrowserRouter} from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import Layout from "./layout/Layout";
 import SearchPage from "./pages/SearchPage/SearchPage";
+import ErrorPage from "./pages/404";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout/>,
+        errorElement: <ErrorPage/>,
         children: [
             {
                 path: '/',
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -3,17 +3,22 @@ import headerStyle from './index.module.css';
 import logo from './logo.png'
 import {Link} from "react-router-dom";
 const Header = () => {
+    const headerItems = [
+        {title: 'Home', to: "/"},
+        {title: 'Search', to: "/search"},
+        {title: 'Live', to: "/live"},
+        {title: 'Music', to: "/music"},
+        {title: 'Charts', to: "/charts"},
+        {title: 'Events', to: "/events"},
+        {title: 'Features', to: "/features"},
+    ]
     return (
         <header className={headerStyle.header}>
             <Link to="/"><img className={headerStyle.logo} src={logo} alt='logo'/></Link>
             <ul className={headerStyle.items}>
-                <li className={headerStyle.item}><Link to="/">Home</Link></li>
-                <li className={headerStyle.item}><Link to="/search">Search</Link></li>
-                <li className={headerStyle.item}><Link to="/live">Live</Link></li>
-                <li className={headerStyle.item}><Link to="/music">Music</Link></li>
-                <li className={headerStyle.item}><Link to="/charts">Charts</Link></li>
-                <li className={headerStyle.item}><Link to="/events">Events</Link></li>
-                <li className={headerStyle.item}><Link to="/features">Features</Link></li>
+                {headerItems.map((h)=>
+                    <li className={headerStyle.item}><Link to={h.to}>{h.title}</Link></li>
+                )}
             </ul>
         </header>
     );
